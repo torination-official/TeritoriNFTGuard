4f7bbc1ddd0dfbbaf78da86e515645fa884a1bbe
torination <robert@wenig.io>
2022-12-02 15:59:39 +0100

    initial commit

---
+++ src/main/java/at/torination/discord/tng/TeritoriNftGuard.java
@@ -0,0 +1,13 @@
+package at.torination.discord.tng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeritoriNftGuard {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeritoriNftGuard.class, args);
+	}
+
+}
---
+++ src/test/java/at/torination/discord/tng/TeritoriNftGuardTests.java
@@ -0,0 +1,13 @@
+package at.torination.discord.tng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeritoriNftGuardTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
